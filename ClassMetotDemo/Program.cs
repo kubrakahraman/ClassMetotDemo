6d74b8533035815aff41a3e9631897770e9c875d
kubrakahraman <77576620+kubrakahraman@users.noreply.github.com>
2021-01-17 23:42:54 +0300

    Add files via upload

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Ad = "Kübra";
+            musteri.Soyad = "Kahraman";
+            musteri.No = 3;
+
+            Musteri musteri2 = new Musteri();
+            musteri.Ad = "Bahar";
+            musteri.Soyad = "Küçükıstıranca";
+            musteri.No = 4;
+
+            Musteri musteri3 = new Musteri();
+            musteri.Ad = "Yasemin";
+            musteri.Soyad = "Keskin";
+            musteri.No = 5;
+
+      
+            Musteri[] musteriler = new Musteri[] { musteri, musteri2, musteri3 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri4 = new Musteri();
+            musteri.Ad = "Bilge";
+            musteri.Soyad = "Dere";
+            musteri.No = 6;
+
+            musteriManager.MusteriEkleme(musteri4);
+            musteriManager.MusteriSilme(musteri3);
+
+            Console.WriteLine("--------Müşteriler----- ");
+            musteriManager.MusteriListeleme(musteriler);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri EKlendi :" + musteri.Ad);
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Ad);
+        }
+        public void MusteriListeleme(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " : " + musteri.No);
+            }
+        }
+    }
+}
